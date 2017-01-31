47d32bab49016c38f04cf957b65d77c512a90a91
aqutw <aquarian.ex+github@gmail.com>
2017-01-31 21:01:25 +0800

    Using Highlight Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { HighlightDirective } from './shared/highlight.directive';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, BookListComponent
+    AppComponent, BookListComponent, HighlightDirective
   ],
   imports: [
     BrowserModule,
