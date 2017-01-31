71cfa12ae49311da125d1c49bf1e5ef772de9f13
aqutw <aquarian.ex+github@gmail.com>
2017-01-31 21:28:57 +0800

    Using Interface

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
 
+interface IBook {
+  bookAuthor: string;
+  bookTitle: string;
+  bookPrice: number;
+  bookDesc: string;
+  publishedOn: Date;
+  isStock: string;
+  bookReviews: number;
+  hardcover?: boolean;
+  bookImageUrl: string;
+}
+
 @Component({
   selector: 'bs-book-list',
   templateUrl: 'book-list.component.html'
 })
 
 export class BookListComponent {
+  books: IBook[] = [{
+    bookAuthor: "string",
+    bookTitle: "string",
+    bookPrice: 33,
+    bookDesc: "string",
+    publishedOn: new Date('02/11/1921'),
+    isStock: "string",
+    bookReviews: 11,
+    hardcover: false,
+    bookImageUrl: "string",
+  }];
   isShowImage: boolean = true;
 
   toggleImage(): void {
