9dc9b2789ce9a8d4ab6a03c48d67e67f9108ccfb
aqutw <aquarian.ex+github@gmail.com>
2017-01-31 21:17:46 +0800

    Using truncate Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TruncatePipe } from './shared/pipes/truncate.pipe';
 import { HighlightDirective } from './shared/highlight.directive';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, BookListComponent, HighlightDirective
+    AppComponent, BookListComponent, HighlightDirective, TruncatePipe
   ],
   imports: [
     BrowserModule,
