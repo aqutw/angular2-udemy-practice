87f40f5f25c069eb171892a9eb52b73f819ea4f9
aqutw <aquarian.ex+github@gmail.com>
2017-02-01 12:24:31 +0800

    change highlight color

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -2,6 +2,6 @@ import { Directive, ElementRef, Input } from '@angular/core';
 @Directive({ selector: '[myHighlight]' })
 export class HighlightDirective {
     constructor(el: ElementRef) {
-       el.nativeElement.style.backgroundColor = 'yellow';
+       el.nativeElement.style.backgroundColor = '#ccc';
     }
 }
